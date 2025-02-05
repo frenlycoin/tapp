89b23a8f35e36c03e35da4e02f10ff041a002da3
Frenly Bro <frenlybro@proton.me>
2025-02-05 21:48:52 +0100

    testing

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ class App {
     }
 
     compound() {
-        app.tg.openTelegramLink('t.me/FrenlyNews/195');
+        Telegram.openTelegramLink('t.me/FrenlyNews/195');
 
         app.tg.SecondaryButton.showProgress(true);
         app.miningRestart = false;
