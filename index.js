172347109c59f440991228c32e28fd28545d1782
Frenly Bro <frenlybro@proton.me>
2025-02-05 23:40:36 +0100

    testing

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ class App {
                 // app.tg.openTelegramLink('https://t.me/FrenlyNews/195');
                 this.tg.close();
             } else {
+                alert("fdfdsa");
                 $("#main").show();
             }
 
@@ -123,12 +124,13 @@ class App {
                 location.reload();
             });
         } catch (e) {
-            // this.tgid = 7422140567;
-            // this.loadData();
-            // $("#first_name").html("Dev");
-            // this.resize();
-            // $("#infoMessage").html("<small><strong>Join <a href=\"https://t.me/FrenlyCoin\" target=\"_blank\" class=\"text-danger\">@FrenlyCoin</a> group for help and support!</strong></small>")
-            // $("#infoMessage").show();
+            this.tgid = 7422140567;
+            this.loadData();
+            $("#first_name").html("Dev");
+            this.resize();
+            $("#infoMessage").html("<small><strong>Join <a href=\"https://t.me/FrenlyCoin\" target=\"_blank\" class=\"text-danger\">@FrenlyCoin</a> group for help and support!</strong></small>")
+            $("#infoMessage").show();
+            $("#main").show();
         }
     }
 
